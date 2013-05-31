dcbe050bebddcc16cd1eebab4fb7e651b674fbe7
imelstorn <imelstorn@gmail.com>
2013-05-31 05:32:10 -0700

    new mail subscr

---
+++ mail66newsVersion/index_mail.php
@@ -0,0 +1,101 @@
+<?php 
+$to  = "imelstorn@gmail.com"; 
+$subject = "Новости 66.ru"; 
+
+$message = '<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+
+<title>Лента новостей рассылка 66</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
+
+</head>
+<body>
+
+	<table class="subscribe__title" style="margin: 0px;border-collapse: collapse;border: none;">
+		<tr>
+			<td class="min" style="width: 1%; padding: 0px; padding-right: 20px; padding-bottom: 20px; vertical-align: middle;">
+				<a href="http://www.66.ru/"><img style="height: 60px;width:119px;display: block;" src="http://s.66.ru/new66/3c/87/06/84/3c870684.gif" alt=""/></a>
+			</td>
+			<td style="padding: 0px;padding-bottom: 20px; vertical-align: middle;">
+				<h1 style="margin: 0px;padding: 0px;font-weight: bold;font-size: 16px;">Новости про адский метеоризм, случившийся недавно в Челябинской области, причинивший колоссальный ущерб Челябинской области. Тольк на 66.ru</h1>
+			</td>
+		</tr>
+	</table>
+	<h2 style="margin: 0px;padding: 0px;font-weight: normal;font-size: 18px;margin-bottom: 10px;">Главные</h2>
+	<table class="subscribe__table" style="	margin: 0px;border-collapse: collapse;border: none;margin-bottom: 20px;">
+		<tr>
+			<td class="min" style="vertical-align: top;width: 1%;padding-right: 20px;padding-bottom: 20px;">
+				<a href="http://www.66.ru/"><img style="height: 60px;display: block;	margin-top: 5px; width: 100px; background: #ccc;" src="http://s.66.ru/new66/news/2e/19/66/7d/2e19667d_resizedScaled_100to66.jpg" alt=""/></a>
+			</td>
+			<td style="vertical-align: top;padding-bottom: 20px;">
+				<h3 style="padding: 0px;margin: 0px;font-weight: normal;margin-bottom: 8px;font-size: 16px;"><a href="http://www.66.ru/">«Форум групп» планирует экспансию в районе «Радуга парка»</a></h3>
+				<p style="padding: 0px;margin: 0px;">Компания выиграла участок<br />земли под строительство<br />большого паркинга. Но никакого отношения к ТРЦ он иметь не будет.</p>
+			</td>
+		</tr>
+		<tr>
+			<td class="min" style="vertical-align: top;width: 1%;padding-right: 20px;padding-bottom: 20px;">
+				<a href="http://www.66.ru/"><img style="height: 60px;display: block;	margin-top: 5px; width: 100px; background: #ccc;" src="http://s.66.ru/new66/news/de/0b/9b/c2/de0b9bc2_resizedScaled_100to66.jpg" alt=""/></a>
+			</td>
+			<td style="vertical-align: top;padding-bottom: 20px;">
+				<h3 style="padding: 0px;margin: 0px;font-weight: normal;margin-bottom: 8px;font-size: 16px;"><a href="http://www.66.ru/">«Форум групп» планирует экспансию в районе «Радуга парка»</a></h3>
+				<p style="padding: 0px;margin: 0px;">Компания выиграла участок земли под строительство большого паркинга. Но никакого отношения к ТРЦ он иметь не будет.</p>
+			</td>
+		</tr>
+		<tr>
+			<td class="min" style="vertical-align: top;width: 1%;padding-right: 20px;padding-bottom: 20px;">
+				<a href="http://www.66.ru/"><img style="height: 60px;display: block;	margin-top: 5px; width: 100px; background: #ccc;" src="http://s.66.ru/new66/news/66/39/2b/3f/66392b3f_resizedScaled_100to75.jpg" alt=""/></a>
+			</td>
+			<td style="vertical-align: top;padding-bottom: 20px;">
+				<h3 style="padding: 0px;margin: 0px;font-weight: normal;margin-bottom: 8px;font-size: 16px;"><a href="http://www.66.ru/">«Форум групп» планирует экспансию в районе «Радуга парка»</a></h3>
+				<p style="padding: 0px;margin: 0px;">Компания выиграла участок земли под строительство большого паркинга. Но никакого отношения к ТРЦ он иметь не будет.</p>
+			</td>
+		</tr>		
+	</table>
+	<h2 style="margin: 0px;padding: 0px;font-weight: normal;font-size: 18px;margin-bottom:10px;">Происшествия</h2>
+	<table class="subscribe__table" style="	margin: 0px;border-collapse: collapse;border: none;margin-bottom: 20px;">
+		<tr>
+			<td class="min" style="vertical-align: top;width: 1%;padding-right: 20px;padding-bottom: 20px;">
+				<a href="http://www.66.ru/"><img style="height: 60px;display: block;	margin-top: 5px; width: 100px; background: #ccc;" src="http://s.66.ru/new66/news/2e/19/66/7d/2e19667d_resizedScaled_100to66.jpg" alt=""/></a>
+			</td>
+			<td style="vertical-align: top;padding-bottom: 20px;">
+				<h3 style="padding: 0px;margin: 0px;font-weight: normal;margin-bottom: 8px;font-size: 16px;"><a href="http://www.66.ru/">«Форум групп» планирует экспансию в районе «Радуга парка»</a></h3>
+				<p style="padding: 0px;margin: 0px;">Компания выиграла участок<br />земли под строительство<br />большого паркинга. Но никакого отношения к ТРЦ он иметь не будет.</p>
+			</td>
+		</tr>
+		<tr>
+			<td class="min" style="vertical-align: top;width: 1%;padding-right: 20px;padding-bottom: 20px;">
+				<a href="http://www.66.ru/"><img style="height: 60px;display: block;	margin-top: 5px; width: 100px; background: #ccc;" src="http://s.66.ru/new66/news/de/0b/9b/c2/de0b9bc2_resizedScaled_100to66.jpg" alt=""/></a>
+			</td>
+			<td style="vertical-align: top;padding-bottom: 20px;">
+				<h3 style="	padding: 0px;margin: 0px;font-weight: normal;margin-bottom: 8px;font-size: 16px;"><a href="http://www.66.ru/">«Форум групп» планирует экспансию в районе «Радуга парка»</a></h3>
+				<p style="padding: 0px;margin: 0px;">Компания выиграла участок земли под строительство большого паркинга. Но никакого отношения к ТРЦ он иметь не будет.</p>
+			</td>
+		</tr>
+		<tr>
+			<td class="min" style="vertical-align: top;width: 1%;padding-right: 20px;padding-bottom: 20px;">
+				<a href="http://www.66.ru/"><img style="height: 60px;display: block;	margin-top: 5px; width: 100px; background: #ccc;" src="http://s.66.ru/new66/news/66/39/2b/3f/66392b3f_resizedScaled_100to75.jpg" alt=""/></a>
+			</td>
+			<td style="vertical-align: top;padding-bottom: 20px;">
+				<h3 style="padding: 0px;margin: 0px;font-weight: normal;margin-bottom: 8px;font-size: 16px;"><a href="http://www.66.ru/">«Форум групп» планирует экспансию в районе «Радуга парка»</a></h3>
+				<p style="padding: 0px;margin: 0px;">Компания выиграла участок земли под строительство большого паркинга. Но никакого отношения к ТРЦ он иметь не будет.</p>
+			</td>
+		</tr>		
+	</table>	
+	
+	<div class="unsubscribe" style="border-top: 1px solid #d8d8d8;padding: 10px 20px 20px;">
+		Для того, чтобы отписаться от данной рассылки, перейдитепо ссылке<br/>
+		<a href="http://www.66.ru/">Вы можете ответить на данное сообщение по email и ответ отобразится новым сообщением в Мегаплане.</a>
+	</div>
+
+	
+</body>
+</html>'; 
+
+$headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+$headers .= "From: Testing system <koropgleb@koropgleb.ru>\r\n"; 
+$headers .= "Bcc: birthday-archive@example.com\r\n"; 
+
+mail($to, $subject, $message, $headers); 
+die("готово");
+?>
---
+++ mail66newsVersion/index_mail1.php
@@ -0,0 +1,167 @@
+<?php 
+$to  = "imelstorn@gmail.com"; 
+$subject = "Новости 66.ru"; 
+
+$message = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+    <title>рассылка 66</title>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+</head>
+<body>
+<style type="text/css">
+* { padding: 0; margin: 0; font-size: 14px; font-family: arial,
+sans-serif; line-height: 1.4; }
+html, body { height: 100%; }
+a { color: #037DD3; text-decoration: none; }
+h1 { font-size: 28px; margin-bottom: 8px; font-weight: normal; }
+h2 { font-size: 21px; margin-bottom: 8px; font-weight: normal; }
+h3 { font-size: 18px; margin-bottom: 8px; font-weight: normal; }
+h4 { font-size: 14px; margin-bottom: 8px; }
+p { font-size: 14px; margin-bottom: 10px; }
+.body ul { margin-left: 32px; }
+.body ul li { padding-bottom: 6px; }
+.content { min-height: 100%; position: relative; width: 800px; margin:
+0 auto; margin-top: 0px; margin-bottom: 60px; }
+.content .head { margin-bottom: 18px; height: 50px; background-color:
+#779f1a; padding-top: 4px; padding-bottom: 4px; }
+.content .head img { margin-left: 5px; margin-right: 10px;}
+.content .head .mdash,
+.content .head .text { position: relative; line-height:50px; color:
+white; font-size: 25px; border: none; font-weight: bold; font-style:
+italic; }
+.content .head a.logo { color: white; border: none; font-weight: bold;
+font-style: italic; text-decoration:none; }
+.content .body { padding: 0 10px; }
+.body .more-news { list-style: none; margin-left: 0; }
+.body .more-news li a { font-size: 12px; }
+.body .more-news li u { color: #7C7C7C; font-size: 11px; margin-right: 12px; }
+.body .newslist-title { margin-top: 20px; margin-bottom: 20px;
+background-color: #519000; padding: 2px 15px 2px 15px; color: white;
+font-size: 24px;}
+.body .newslist-title a { color: white; border-bottom: none; font-size: 24px; }
+.body .news-list ul { list-style: none; margin-left: 0; }
+.body .news-list ul li { padding-bottom: 10px; }
+.body .news-list ul li td { vertical-align: top; }
+.body .news-list ul a { font-size: 18px; }
+.body .news-list ul .news_single_heading { position: relative; top: -9px; }
+.body .news-list ul .news_single__content { padding-left: 13px;
+margin-left: 118px; }
+.body .news-list ul .news_single_date { color: #666; font-size: 11px;
+font-style: normal; }
+.body .news-list ul .news_single_annotation { margin-top: -9px;
+font-size: 13px; margin-bottom: 0; }
+.body .news_single_image a { border: none; }
+.body .news_single_image img { border: none; }
+.objectlink{ color:#7c7c7c; font-size:10px; }
+.foot { height: 40px; background-color: #779f1a; padding: 10px; }
+.foot-content {margin:0 auto; height: 40px; position: relative; width: 800px;}
+.foot { position: relative; }
+.foot img { margin-left: 12px; margin-right: 10px; }
+.foot .text { color: #CEDCAC;}
+.foot a { color: #CEDCAC; border-bottom: 1px solid #CEDCAC;}
+</style>
+
+<div class="content">
+	<div style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;margin-bottom:18px;min-height:50px;background-color:#779f1a;padding-top:4px;padding-bottom:4px">
+		<table border="0" cellpadding="0" cellspacing="0" width="100%" style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4"><tbody style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+			<tr style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+				<td width="1" style="padding:10px 10px;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4; vertical-align: middle;">
+					<a href="http://66.ru/" style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;color:white;text-decoration:none;border:none;font-weight:bold;font-style:italic" target="_blank">
+						<img alt="66.ru" width="93" height="47" title="66.ru" src="http://s.66.ru/new66/3c/87/06/84/3c870684.gif" style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;margin-left:5px;margin-right:10px; background: #ccc; display: block;">
+					</a>
+				</td>
+				<td style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height: 1.4; vertical-align: middle;">
+					<span style="padding:0;margin:0;font-size:25px;font-family:arial,sans-serif;line-height:18px;color:white;border:none;font-weight:bold;font-style:italic">—</span> <a href="http://66.ru/" style="padding:0;margin:0;font-size:25px;font-family:arial,sans-serif;line-height: 24px;color:white;text-decoration:none;border:none;font-weight:bold;font-style:italic" target="_blank"> Достоверно и подробно о метеоритной атаке на Урал</a>
+				</td>
+			</tr>
+		</tbody></table>
+	</div>
+	<div class="body">
+
+		<p style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;margin-bottom:20px">Уважаемые читатели, мы были одними из первых кто написал о метеоритном дожде, который обрушился на Свердловскую и Челябинскую область. Весь день мы собирали по кускам достоверную информацию о космической катастрофе и публиковали ее у себя на Портале 66.ru. Читайте подборку лучших материалов по теме. Факты, свидетельства очевидцев, фото, видео, прогнозы.</p>
+
+
+		<div style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+			<ul style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;margin-left:0;list-style:none">
+				<li style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;padding-bottom:10px">
+					<table style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4"><tbody style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+						<tr style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+							<td style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;vertical-align:top">
+								<a href="http://66.ru/news/society/132207/" style="padding:0;margin:0;font-size:18px;font-family:arial,sans-serif;line-height:1.4;color:#037dd3;text-decoration:none;border:none" target="_blank"> 
+									<img width="100" height="90" style="background: #ccc; padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;border:none">
+								</a>
+							</td>
+						<td style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;vertical-align:top;padding-left:13px;margin-left:118px">
+							<div style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+								<div style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+									<a href="http://66.ru/news/society/132207/" style="padding:0;margin:0;font-size:18px;font-family:arial,sans-serif;line-height:1.4;color:#037dd3;" target="_blank">Хроника пикирующего метеорита. Сборка фото, видео, фактов</a>
+								</div>
+								<i style="padding:0;margin:0;font-size:11px;font-family:arial,sans-serif;line-height:1.4;color:#666;font-style:normal">15.02.2013 17:30</i>&nbsp;&nbsp;</div>
+							<p style="padding:0;margin:0;font-size:13px;font-family:arial,sans-serif;line-height:1.4;margin-bottom:0">Из массивного потока противоречивой информации мы выбрали для вас только самое важное и интересное.</p>
+						</td>
+						</tr>
+					</table>
+				</li>
+				<li style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;padding-bottom:10px">
+					<table style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4"><tbody style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+						<tr style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+							<td style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;vertical-align:top">
+								<a href="http://66.ru/news/society/132207/" style="padding:0;margin:0;font-size:18px;font-family:arial,sans-serif;line-height:1.4;color:#037dd3;border:" target="_blank"> 
+									<img width="100" height="90" style="background: #ccc;padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;border:none">
+								</a>
+							</td>
+						<td style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;vertical-align:top;padding-left:13px;margin-left:118px">
+							<div style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+								<div style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+									<a href="http://66.ru/news/society/132207/" style="padding:0;margin:0;font-size:18px;font-family:arial,sans-serif;line-height:1.4;color:#037dd3;" target="_blank">Хроника пикирующего метеорита. Сборка фото, видео, фактов</a>
+								</div>
+								<i style="padding:0;margin:0;font-size:11px;font-family:arial,sans-serif;line-height:1.4;color:#666;font-style:normal">15.02.2013 17:30</i>&nbsp;&nbsp;</div>
+							<p style="padding:0;margin:0;font-size:13px;font-family:arial,sans-serif;line-height:1.4;margin-bottom:0">Из массивного потока противоречивой информации мы выбрали для вас только самое важное и интересное.</p>
+						</td>
+						</tr>
+					</table>
+				</li>
+				<li style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;padding-bottom:10px">
+					<table style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4"><tbody style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+						<tr style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+							<td style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;vertical-align:top">
+								<a href="http://66.ru/news/society/132207/" style="padding:0;margin:0;font-size:18px;font-family:arial,sans-serif;line-height:1.4;color:#037dd3;" target="_blank"> 
+									<img width="100" height="90" style="background: #ccc; padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;border:none">
+								</a>
+							</td>
+						<td style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4;vertical-align:top;padding-left:13px;margin-left:118px">
+							<div style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+								<div style="padding:0;margin:0;font-size:14px;font-family:arial,sans-serif;line-height:1.4">
+									<a href="http://66.ru/news/society/132207/" style="padding:0;margin:0;font-size:18px;font-family:arial,sans-serif;line-height:1.4;color:#037dd3;" target="_blank">Хроника пикирующего метеорита. Сборка фото, видео, фактов</a>
+								</div>
+								<i style="padding:0;margin:0;font-size:11px;font-family:arial,sans-serif;line-height:1.4;color:#666;font-style:normal">15.02.2013 17:30</i>&nbsp;&nbsp;</div>
+							<p style="padding:0;margin:0;font-size:13px;font-family:arial,sans-serif;line-height:1.4;margin-bottom:0">Из массивного потока противоречивой информации мы выбрали для вас только самое важное и интересное.</p>
+						</td>
+						</tr>
+					</table>
+				</li>				
+			</ul>
+		</div>
+
+	</div>
+	
+	<div class="unsubscribe" style="border-top: 1px solid #d8d8d8;padding: 10px 20px 20px;">
+		Для того, чтобы отписаться от данной рассылки, перейдитепо ссылке<br/>
+		<a href="">Вы можете ответить на данное сообщение по email и ответ отобразится новым сообщением в Мегаплане.</a>
+	</div>
+	
+	
+</div>
+
+</body>
+</html>'; 
+
+$headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+$headers .= "From: Testing system <koropgleb@koropgleb.ru>\r\n"; 
+$headers .= "Bcc: birthday-archive@example.com\r\n"; 
+
+mail($to, $subject, $message, $headers); 
+die("готово");
+?>
