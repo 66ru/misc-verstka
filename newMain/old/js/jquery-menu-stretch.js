b3e2958ac7993c3fc22ed99d09219655aebad956
imelstorn <imelstorn@gmail.com>
2013-08-05 21:56:23 +0600

    new main

---
+++ newMain/old/js/jquery-menu-stretch.js
@@ -0,0 +1,43 @@
+	var letterWidth = 8; //ширина строчной буквы шрифта в меню
+	
+	function skukogit() {
+		var menuSize = $(".b-header-menu > li").not(".hasSub").size() - 1; //количество пунктов
+		element = $(".b-header-menu li").eq(menuSize).html();
+		$(".b-header-menu li").eq(menuSize).remove();
+		$(".b-header-menu__submenu li").eq(0).before("<li>" + element + "</li>");
+	}
+	
+	function raskukogit() {
+		var colSize = $(".js-main-col").width();
+		var logoSize = $(".b-header-logo").width();			
+		var menuWidth = $(".js-menu-width").width();
+		var menuSize = $(".b-header-menu > li").not(".hasSub").size() - 1; //количество пунктов
+		lastHiddenElementMenu = $(".b-header-menu__submenu > li").eq(0);//элемент, который надо вернуть в меню 1 уровня
+		var placeWidth = lastHiddenElementMenu.text().length * letterWidth + 20; //ширина вакантного места для возврата
+		if ((logoSize + menuWidth + 45 + placeWidth) <= colSize) {//если слово влезает на вакантное место
+			$(".b-header-menu li").eq(menuSize).after("<li>" + lastHiddenElementMenu.html() + "</li>");
+			lastHiddenElementMenu.remove();
+		}
+	}
+	
+	function checkMenu() {
+		var colSize = $(".js-main-col").width();
+		var logoSize = $(".b-header-logo").width();
+		var menuSize = $(".js-menu-width").width();			
+		if (($(".b-header-logo").width() + $(".js-menu-width").width() + 45) > $(".js-main-col").width()) {
+			do {
+				skukogit();
+			} while (($(".b-header-logo").width() + $(".js-menu-width").width() + 45) > $(".js-main-col").width());
+		} else {
+			raskukogit();
+		}
+	}
+	
+$(document).ready(function() {
+	checkMenu();
+});	
+	
+	
+$(window).resize(function() {
+	checkMenu();
+}) 	
