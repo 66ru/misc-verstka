a55c983e7a721a6dcf726e89537cc54bf642ce96
imelstorn <imelstorn@gmail.com>
2013-08-21 16:01:19 +0600

    новая верстка шапки и футера

---
+++ newMain/js/pageScript.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+		
+		
+	function fixMiddleBanner() {
+		$(".js-banner-middle-layout").width($(".js-etalon-2col-width").width() + "px");
+	}
+	
+	
+	$(".b-ad-btn").hover(function() {
+		$(this).animate({width: '80px'}, 100);
+	}, function() {
+		$(this).animate({width: '18px'}, 100);
+	});
+	
+
+	
+	
+	
+	
+	
+	var i = 0;
+	var letterWidth = 8;
+	
+	fixMiddleBanner();
+	
+
+	
+	
+	
+
+	
+	$(window).resize(function() {
+		fixMiddleBanner();
+	}) 
+		
+	
+	$(".js-tele-afisha-link-more").click(function() {
+		$(".js-tele-afisha-hidden").toggle();
+		$(".js-tele-afisha-link-more").toggle();
+		return false;
+	});
+	
+
+
+	
+		
+		
+		
+});
