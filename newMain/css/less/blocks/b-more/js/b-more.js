5d55d7ff0db6c8a7d60d91842f8e2b875b7c4762
Imelstorn <imelstorn@gmail.com>
2013-10-22 17:26:32 +0600

    1. фикс сетки на главной (колоночность) 2. доделки по банковским валютам 3. кнопки с css на less (мой парсер не понимает (css) в dep.)

--- newMain/css/less/blocks/b-more/js/b-more.js
+++ newMain/css/less/blocks/b-more/js/b-more.js
@@ -14,23 +14,6 @@ $(document).ready(function() {
 		
 		$(this).parents(".b-more").find(".b-more__visible").attr("href", thisHref);
 		$(this).parents(".b-more").find(".b-more__visible").html(thisTitle);
-
-		
-		/*var massItem = [];
-		var parentTree = $(this).parents(".b-more__popup");
-		var i = 0;
-		
-		parentTree.find(".b-more__elem > a").each(function() {
-			massItem[i] = "[" + $(this).html() + "," + $(this).attr("href") + "]";
-			i++;
-		});
-		
-		var curItemIndex = $(this).index();
-		
-		var outText = $(this).parent().find(".b-more__elem").eq(curItemIndex).find("a").html();
-		var outUrl = $(this).parent().find(".b-more__elem").eq(curItemIndex).find("a").attr("href");*/
-		//alert(curItemIndex);
-		
 		
 	});
 	
