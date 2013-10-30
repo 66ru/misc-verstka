c34db2374d64fc2f8bf170f97bc35c70759c56dc
Anton Belyaev <anton.n.belyaev@gmail.com>
2013-10-30 13:44:16 +0600

    New doska-card: disable sticky blocks on mobile

--- newMain/css/less/blocks/b-doska-ncard/js/sticky-info.js
+++ newMain/css/less/blocks/b-doska-ncard/js/sticky-info.js
@@ -1,76 +1,98 @@
-jQuery(window).on('load', function () {
-	$('.b-doska-ncard__info').each(function() {
+(function() {
+var isMobile = {
+	Android: function() {
+		return navigator.userAgent.match(/Android/i) ? true : false;
+	},
+	BlackBerry: function() {
+		return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+	},
+	iOS: function() {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+	},
+	Windows: function() {
+		return navigator.userAgent.match(/IEMobile/i) ? true : false;
+	},
+	any: function() {
+		return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+	}
+};
 
-		var $window = $(window),
-			$sticky = $(this),
-			$parent = $sticky.parents('.b-doska-ncard'),
-			$content = $parent.children('.b-doska-ncard__content'),
+if (!isMobile.any()) {
+	jQuery(window).on('load', function () {
+		$('.b-doska-ncard__info').each(function() {
 
-			sticky = {},
-			parent = {},
-			state;
+			var $window = $(window),
+				$sticky = $(this),
+				$parent = $sticky.parents('.b-doska-ncard'),
+				$content = $parent.children('.b-doska-ncard__content'),
 
-		getParams();
-		repos($window.scrollTop());
+				sticky = {},
+				parent = {},
+				state;
 
-		$window.on('scroll', function() {
-			repos($window.scrollTop());
-		});
-
-		$window.on('resize', function() {
-			makeStatic();
 			getParams();
 			repos($window.scrollTop());
+
+			$window.on('scroll', function() {
+				repos($window.scrollTop());
+			});
+
+			$window.on('resize', function() {
+				makeStatic();
+				getParams();
+				repos($window.scrollTop());
+			});
+
+			$sticky.on('click', function() {
+				sticky.height = $sticky.outerHeight();
+				repos($window.scrollTop());
+			});
+
+			function isStickable() {
+				if (sticky.height < parent.contentHeight) { return true; }
+			}
+
+			function getParams() {
+				sticky.top = $sticky.offset().top;
+				sticky.left = $sticky.offset().left;
+				sticky.width = $sticky.outerWidth();
+				sticky.height = $sticky.outerHeight();
+				sticky.relLeft = $sticky.position().left;
+
+				parent.top = $parent.offset().top;
+				parent.height = $parent.outerHeight();
+				parent.bottom = parent.top + parent.height;
+				parent.contentHeight = $content.outerHeight();
+			}
+
+			function makeStatic() {
+				state = 'static';
+				$sticky.removeClass('b-doska-ncard__info_sticky_top')
+					.removeClass('b-doska-ncard__info_sticky_parent-bottom');
+			}
+
+			function makeSticky() {
+				state = 'sticky';
+				$sticky.removeClass('b-doska-ncard__info_sticky_parent-bottom')
+					.addClass('b-doska-ncard__info_sticky_top');
+				$sticky.css({left: sticky.left});
+			}
+
+			function makeAbsolute() {
+				state = 'parent-bottom';
+				$sticky.removeClass('b-doska-ncard__info_sticky_top')
+					.addClass('b-doska-ncard__info_sticky_parent-bottom');
+				$sticky.css({left: sticky.relLeft});
+			}
+
+			function repos(windowScrollTop) {
+				if ( (sticky.top < windowScrollTop) && isStickable() ) {
+					if (windowScrollTop + sticky.height < parent.bottom) {
+						if (state !== 'sticky') { makeSticky(); }
+					} else if (state !== 'parent-bottom') { makeAbsolute(); }
+				} else if (state !== 'static') { makeStatic(); }
+			}
 		});
-
-		$sticky.on('click', function() {
-			sticky.height = $sticky.outerHeight();
-			repos($window.scrollTop());
-		});
-
-		function isStickable() {
-			if (sticky.height < parent.contentHeight) { return true; }
-		}
-
-		function getParams() {
-			sticky.top = $sticky.offset().top;
-			sticky.left = $sticky.offset().left;
-			sticky.width = $sticky.outerWidth();
-			sticky.height = $sticky.outerHeight();
-			sticky.relLeft = $sticky.position().left;
-
-			parent.top = $parent.offset().top;
-			parent.height = $parent.outerHeight();
-			parent.bottom = parent.top + parent.height;
-			parent.contentHeight = $content.outerHeight();
-		}
-
-		function makeStatic() {
-			state = 'static';
-			$sticky.removeClass('b-doska-ncard__info_sticky_top')
-				.removeClass('b-doska-ncard__info_sticky_parent-bottom');
-		}
-
-		function makeSticky() {
-			state = 'sticky';
-			$sticky.removeClass('b-doska-ncard__info_sticky_parent-bottom')
-				.addClass('b-doska-ncard__info_sticky_top');
-			$sticky.css({left: sticky.left});
-		}
-
-		function makeAbsolute() {
-			state = 'parent-bottom';
-			$sticky.removeClass('b-doska-ncard__info_sticky_top')
-				.addClass('b-doska-ncard__info_sticky_parent-bottom');
-			$sticky.css({left: sticky.relLeft});
-		}
-
-		function repos(windowScrollTop) {
-			if ( (sticky.top < windowScrollTop) && isStickable() ) {
-				if (windowScrollTop + sticky.height < parent.bottom) {
-					if (state !== 'sticky') { makeSticky(); }
-				} else if (state !== 'parent-bottom') { makeAbsolute(); }
-			} else if (state !== 'static') { makeStatic(); }
-		}
 	});
-});
+}
+})();
--- newMain/css/less/blocks/b-sticky-right-col/js/sticky-right-col.js
+++ newMain/css/less/blocks/b-sticky-right-col/js/sticky-right-col.js
@@ -1,85 +1,107 @@
-jQuery(window).on('load', function () {
-	$('.b-sticky-right-col__sticky').each(function() {
+(function() {
+var isMobile = {
+	Android: function() {
+		return navigator.userAgent.match(/Android/i) ? true : false;
+	},
+	BlackBerry: function() {
+		return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+	},
+	iOS: function() {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+	},
+	Windows: function() {
+		return navigator.userAgent.match(/IEMobile/i) ? true : false;
+	},
+	any: function() {
+		return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+	}
+};
 
-		var $window = $(window),
-			$sticky = $(this),
-			$parent = $sticky.parents('.b-sticky-right-col'),
+if (!isMobile.any()) {
+	jQuery(window).on('load', function () {
+		$('.b-sticky-right-col__sticky').each(function() {
 
-			sticky = {},
-			parent = {},
-			state;
+			var $window = $(window),
+				$sticky = $(this),
+				$parent = $sticky.parents('.b-sticky-right-col'),
 
-		getParams();
-		repos($window.scrollTop());
+				sticky = {},
+				parent = {},
+				state;
 
-		$window.on('scroll', function() {
-			repos($window.scrollTop());
-		});
-
-		$window.on('resize', function() {
-			makeStatic();
 			getParams();
 			repos($window.scrollTop());
+
+			$window.on('scroll', function() {
+				repos($window.scrollTop());
+			});
+
+			$window.on('resize', function() {
+				makeStatic();
+				getParams();
+				repos($window.scrollTop());
+			});
+
+			$sticky.on('click', function() {
+				sticky.height = $sticky.outerHeight();
+				repos($window.scrollTop());
+			});
+
+			function isStickable() {
+				if (sticky.height < parent.contentHeight) { return true; }
+			}
+
+			function getParams() {
+				sticky.top = $sticky.offset().top;
+				sticky.left = $sticky.offset().left;
+				sticky.width = $sticky.outerWidth();
+				sticky.height = $sticky.outerHeight();
+				sticky.relLeft = $sticky.position().left;
+
+				parent.top = $parent.offset().top;
+				parent.height = $parent.outerHeight();
+				parent.bottom = parent.top + parent.height;
+				parent.contentHeight = $parent.height();
+			}
+
+			function makeStatic() {
+				state = 'static';
+				$sticky.removeClass('b-sticky-right-col_sticky_top');
+				$sticky.css({
+					top: '',
+					left: '',
+					width: ''
+				});
+			}
+
+			function makeSticky() {
+				state = 'sticky';
+				$sticky.addClass('b-sticky-right-col_sticky_top');
+				$sticky.css({
+					top: '',
+					left: sticky.left,
+					width: sticky.width
+				});
+			}
+
+			function makeStickyBottom() {
+				state = 'parent-bottom';
+				$sticky.removeClass('b-sticky-right-col_sticky_top');
+				$sticky.css({
+					top: parent.height - sticky.height + 15,
+					left: sticky.relLeft,
+					width: sticky.width
+				});
+			}
+
+			function repos(windowScrollTop) {
+				if ( (sticky.top - 15 < windowScrollTop) && isStickable() ) {
+					if (windowScrollTop + sticky.height < parent.bottom) {
+						if (state !== 'sticky') { makeSticky(); }
+					} else if (state !== 'parent-bottom') { makeStickyBottom(); }
+				} else if (state !== 'static') { makeStatic(); }
+			}
 		});
-
-		$sticky.on('click', function() {
-			sticky.height = $sticky.outerHeight();
-			repos($window.scrollTop());
-		});
-
-		function isStickable() {
-			if (sticky.height < parent.contentHeight) { return true; }
-		}
-
-		function getParams() {
-			sticky.top = $sticky.offset().top;
-			sticky.left = $sticky.offset().left;
-			sticky.width = $sticky.outerWidth();
-			sticky.height = $sticky.outerHeight();
-			sticky.relLeft = $sticky.position().left;
-
-			parent.top = $parent.offset().top;
-			parent.height = $parent.outerHeight();
-			parent.bottom = parent.top + parent.height;
-			parent.contentHeight = $parent.height();
-		}
-
-		function makeStatic() {
-			state = 'static';
-			$sticky.removeClass('b-sticky-right-col_sticky_top');
-			$sticky.css({
-				top: '',
-				left: '',
-				width: ''
-			});
-		}
-
-		function makeSticky() {
-			state = 'sticky';
-			$sticky.addClass('b-sticky-right-col_sticky_top');
-			$sticky.css({
-				top: '',
-				left: sticky.left,
-				width: sticky.width
-			});
-		}
-
-		function makeStickyBottom() {
-			state = 'parent-bottom';
-			$sticky.removeClass('b-sticky-right-col_sticky_top');
-			$sticky.css({
-				top: parent.height - sticky.height + 15,
-				left: sticky.relLeft,
-				width: sticky.width
-			});
-		}
-
-		function repos(windowScrollTop) {
-			if ( (sticky.top - 15 < windowScrollTop) && isStickable() ) {
-				if (windowScrollTop + sticky.height < parent.bottom) {
-					if (state !== 'sticky') { makeSticky(); }
-				} else if (state !== 'parent-bottom') { makeStickyBottom(); }
-			} else if (state !== 'static') { makeStatic(); }
-		}
 	});
-});
+}
+})();
