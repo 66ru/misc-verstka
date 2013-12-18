74e08501ce04c9048b4cf78170332fcdb88daca9
Anton Belyaev <anton.n.belyaev@gmail.com>
2013-12-18 17:45:46 +0600

    ATM page: attach ng-bootstrap for suggests box

--- newMain/js/banks-atm-controllers.js
+++ newMain/js/banks-atm-controllers.js
@@ -1,7 +1,12 @@
-var atmApp = angular.module('atmApp', []);
-
-atmApp.controller('ATMListCtrl', function ($scope, $http) {
-	$http.get('data/atmlist.json').success(function(data) {
-		$scope.atms = data;
-	});
-});
+angular.module('atmApp', ['ui.bootstrap'])
+	.controller('BankListCtrl', ['$scope', '$http', function ($scope, $http) {
+		$http.get('data/atmlist.json').success(function(data) {
+			$scope.atms = data;
+		});
+		$http.get('data/banklist.json').success(function(banks) {
+			$scope.banks = banks;
+			$scope.tpy = function() {
+				$scope.selected = $scope.bankQuery;
+			};
+		});
+	}]);
