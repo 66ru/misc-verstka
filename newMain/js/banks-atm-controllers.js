dda22a4285e509c8c34352b2d48b3f0c354b091e
Anton Belyaev <anton.n.belyaev@gmail.com>
2013-12-20 18:31:31 +0600

    ATM list: improve map behaviour

--- newMain/js/banks-atm-controllers.js
+++ newMain/js/banks-atm-controllers.js
@@ -8,10 +8,17 @@ angular.module('atms', ['ui.bootstrap'])
 		});
 		$http.get('data/banks.json').success(function(banks) {
 			$scope.banks = banks;
+
 			$scope.showThisBank = function() {
 				if (!~banksToShow.indexOf($scope.bankQuery.name)) {
 					banksToShow.push($scope.bankQuery.name);
-					console.log(banksToShow);
+				}
+			};
+
+			$scope.hideThisBank = function(name) {
+				var index = banksToShow.indexOf(name);
+				if (name !== -1) {
+					banksToShow.splice(index, 1);
 				}
 			};
 
@@ -19,12 +26,21 @@ angular.module('atms', ['ui.bootstrap'])
 				if (banksToShow.length === 0) return true;
 				return banksToShow.indexOf(elem.bank) >= 0;
 			};
+
+			$scope.bankTagShow = function(elem) {
+				return banksToShow.indexOf(elem) >= 0;
+			};
+
+			$scope.showBanksLabel = function() {
+				return banksToShow.length > 0;
+			}
 		});
 	}])
 
 	.directive('yamElem', function() {
 		function link(scope, element, attrs) {
 			var placemark;
+			// scope.iindex = scope.$index;
 
 			$(function() {
 				ymaps.ready(function() {
@@ -35,6 +51,18 @@ angular.module('atms', ['ui.bootstrap'])
 						iconColor: '#587f02'
 					});
 					clusterer.add(placemark);
+
+					element.on('click', function() {
+						// map.setCenter(placemark.getCoordinates());
+						console.log(placemark.properties.get('iconContent'));
+					});
+
+					// Make placemark label to correspond with label's index
+					scope.$watch(function() {
+						return scope.$index + 1 == placemark.properties.get('iconContent');
+					}, function() {
+						placemark.properties.set({iconContent: scope.$index + 1});
+					});
 				});
 			});
 
