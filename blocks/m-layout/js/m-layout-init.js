a97d51b31b5ce9bd8cf2dd5913f7e185224b1815
Anton Belyaev <anton.n.belyaev@gmail.com>
2014-02-20 17:08:06 +0600

    Mobile mobile mobile

--- blocks/m-layout/js/m-layout-init.js
+++ blocks/m-layout/js/m-layout-init.js
@@ -1,7 +1,6 @@
 (function() {
 	$(function() {
-		var $window = $(window),
-			$menuButton = $('.m-menu__button'),
+		var $menuButton = $('.m-menu__button'),
 			$mainContainer = $('.m-layout'),
 			$closeGlobal = $('.m-layout__close-global'),
 
@@ -9,14 +8,31 @@
 			closeGlobalOnClass = 'm-layout__close-global_on';
 
 		$menuButton.on('click', function() {
+			$('.m-sidemenu').show();
 			$mainContainer.toggleClass(menuShowMenuClass);
 			$closeGlobal.addClass(closeGlobalOnClass);
-			// $(window).scrollTop(0);
 		});
 
 		$closeGlobal.on('click', function() {
 			$mainContainer.removeClass(menuShowMenuClass);
 			$closeGlobal.removeClass(closeGlobalOnClass);
+		});
+	});
+})();
+
+
+(function() {
+	$(function() {
+		$('.m-new-comment').hide();
+		$('.m-authorize__button').on('click', function() {
+			$('.m-authorize').hide();
+			$('.m-new-comment').show();
+			return false;
+		});
+		$('.m-new-comment__close').on('click', function() {
+			$('.m-new-comment').hide();
+			$('.m-authorize').show();
+			return false;
 		});
 	});
 })();
