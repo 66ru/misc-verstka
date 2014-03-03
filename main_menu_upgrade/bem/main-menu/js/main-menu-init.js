232c79565250ffc5535b38c496a95fecbcc4fdd7
Anton Belyaev <anton.n.belyaev@gmail.com>
2014-03-03 16:34:50 +0600

    Make submenu open on delay without menu-aim

--- main_menu_upgrade/bem/main-menu/js/main-menu-init.js
+++ main_menu_upgrade/bem/main-menu/js/main-menu-init.js
@@ -1,70 +1,60 @@
 (function($) {
 	$(function() {
-		var	domHeader = $('.l-header-wrap').first(),
-			domMenu = domHeader.find('.main-menu').first(),
-			domElems = domMenu.find('.main-menu__elem'),
-			domMore = domMenu.find('.main-menu__more'),
-			domGlobalWidthSubs = domMenu.find('.main-menu__sub_width_global'),
-			domMoreSubElems = $('.flat-sub__elem'),
+		var	$header = $('.l-header-wrap').first(),
+			$menu = $header.find('.main-menu').first(),
+			$elems = $menu.find('.main-menu__elem'),
+			$more = $menu.find('.main-menu__more'),
+			$globalWidthSubs = $menu.find('.main-menu__sub_width_global'),
+			$moreSubElems = $('.flat-sub__elem'),
 
 			menuWidth, moreWidth,
-			widths = [], classes = {};
+			widths = [], classes = {},
+
+			SUBMENU_SHOW_DELAY = 250;
 
 		classes.currentElem = 'main-menu__elem_current';
 		classes.menuNoJs = 'main-menu_js_no';
 
-		function activate(row) {
-			var $this = $(row);
-			$this.addClass(classes.currentElem);
-		}
-
-		function deactivate(row) {
-			var $this = $(row);
-			$this.removeClass(classes.currentElem);
-		}
-
 		function getElemWidths() {
-			domElems.each(function() {
+			$elems.each(function() {
 				widths.push($(this).outerWidth());
 			});
 		}
 
 		function getMenuWidth() {
-			menuWidth = domMenu.width();
+			menuWidth = $menu.width();
 		}
 
 		function initMenu() {
-			domMenu.removeClass(classes.menuNoJs);
-			moreWidth = domMore.outerWidth() + 11; // + arrow image width
+			$menu.removeClass(classes.menuNoJs);
+			moreWidth = $more.outerWidth() + 11; // + arrow image width
 		}
 
 
 		function setMenuLength() {
 			var curWidth = moreWidth;
 
-			domElems.each(function(i) {
+			$elems.each(function(i) {
 				var $this = $(this);
 				curWidth += $this.width();
 
-				if (i + 1 == domElems.length) { return; }
+				if (i + 1 == $elems.length) { return; }
 
 				if (curWidth > menuWidth) {
 					$this.hide();
-					domMoreSubElems.eq(i).show();
+					$moreSubElems.eq(i).show();
 				} else {
 					$this.show();
-					domMoreSubElems.eq(i).hide();
+					$moreSubElems.eq(i).hide();
 				}
 			});
 		}
 
-
 		function setGlobalsWidths() {
-			domGlobalWidthSubs
-				.width(domHeader.width())
-					.css({left: -domMenu.offset().left});
+			$globalWidthSubs
+				.width($header.width())
+					.css({left: -$menu.offset().left});
 		}
-
 
 		initMenu();
 		getMenuWidth();
@@ -72,15 +62,28 @@
 		setMenuLength();
 		setGlobalsWidths();
 
-		domMenu.menuAim({
-			activate: activate,
-			deactivate: deactivate,
-		});
-
 		$(window).on('resize', function() {
 			getMenuWidth();
 			setMenuLength();
 			setGlobalsWidths();
+		});
+
+		$elems
+			.on('mouseenter', function() {
+				var $this = $(this);
+
+				$menu.data('timer', setTimeout(function() {
+					$elems.removeClass(classes.currentElem);
+					$this.addClass(classes.currentElem);
+				}, SUBMENU_SHOW_DELAY));
+			})
+			.on('mouseleave', function() {
+				clearTimeout($menu.data('timer'));
+			});
+
+		$menu.on('mouseleave', function() {
+			clearTimeout($menu.data('timer'));
+			$elems.removeClass(classes.currentElem);
 		});
 	});
 })(jQuery);
