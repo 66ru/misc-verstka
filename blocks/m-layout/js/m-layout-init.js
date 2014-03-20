3c44811c7bdb72138c35f3460f62518e18381cc3
Anton Belyaev <anton.n.belyaev@gmail.com>
2014-03-20 14:22:24 +0600

    Mobile: introduce spinner block

--- blocks/m-layout/js/m-layout-init.js
+++ blocks/m-layout/js/m-layout-init.js
@@ -128,9 +128,16 @@
 		$('.m-comment, .m-authorize').hide();
 
 		$('#show-comments').on('click', function(ev) {
+			var $this = $(this);
 			ev.preventDefault();
-			$(this).addClass('m-button_activated');
-			$('.m-comment, .m-authorize').show();
+			$this.addClass('m-button_loading');
+
+			setTimeout(function() {
+				console.log('bang!');
+				$this.removeClass('m-button_loading');
+				$this.addClass('m-button_activated');
+				$('.m-comment, .m-authorize').show();
+			}, 2000);
 		});
 	});
 })();
