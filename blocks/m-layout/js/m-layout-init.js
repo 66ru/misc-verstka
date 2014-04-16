c5573e57a205cc742807662dc7fc7fc92760c2f3
Anton Belyaev <anton.n.belyaev@gmail.com>
2014-04-16 21:23:05 +0600

    Mobile: apply new layout

--- blocks/m-layout/js/m-layout-init.js
+++ blocks/m-layout/js/m-layout-init.js
@@ -1,118 +1,53 @@
 (function() {
 	$(function() {
-		var $menuButton = $('.m-menu__button'),
-			$mainContainer = $('.m-layout'),
-			$closeGlobal = $('.m-layout__close-global'),
+		var layoutClass = 'm-layout',
+			layoutMainClass = layoutClass + '__main',
+			layoutMainOpenMenuClass = layoutMainClass + '_menu-open',
+			layoutMenuWrapClass = layoutClass + '__menu-wrap',
+			layoutMenuToggleClass = layoutClass + '__toggle-menu',
+			layoutOverlayClass = layoutClass + '__overlay',
 
-			domMainContainer = $mainContainer[0],
+			$layoutMain = $('.' + layoutMainClass),
+			$layoutMenuWrap = $('.' + layoutMenuWrapClass),
+			$layoutMenuToggle = $('.' + layoutMenuToggleClass),
+			$layoutOverlay = $('.' + layoutOverlayClass),
 
-			menuIsVisibleClass = 'm-layout_show-menu_yes',
-			transitionClass = 'm-layout_transition',
-			closeGlobalOnClass = 'm-layout__close-global_on',
+			isMenuOpen = false,
+			MENU_TRANSITION_TIME = 400;
 
-			viewportWidth = $(window).width(),
-			layoutWidthChangePoint = 319;
+			console.log($layoutOverlay)
 
-
-		$menuButton.on('click', function() {
-			$mainContainer.toggleClass(menuIsVisibleClass);
-			$closeGlobal.addClass(closeGlobalOnClass);
-		});
-
-		$closeGlobal.on('click', function() {
-			$mainContainer.removeClass(menuIsVisibleClass);
-			$closeGlobal.removeClass(closeGlobalOnClass);
-		});
-
-
-		var hammerOptions = {
-			drag: true,
-			drag_lock_to_axis: true,
-			drag_min_distance: 15
-		};
-
-		var isDragging = false,
-			scrollTopBeforeDrag;
-
-		Hammer(domMainContainer, hammerOptions).on('dragright dragleft swipeleft swiperight', function(e) {
-			var deltaX = e.gesture.deltaX,
-				currentX = 0,
-				newX = 0;
-
-			isDragging = true;
-			e.gesture.preventDefault();
-			if (e.type == 'swipeleft' || e.type == 'swiperight') { return; }
-
-			$mainContainer.removeClass(transitionClass);
-
-			if (!$mainContainer.hasClass(menuIsVisibleClass)) {
-				currentX = 0;
-			} else currentX = 260;
-
-			newX = currentX + deltaX;
-
-			if (newX > 260) newX = 260;
-			if (newX < 0) newX = 0;
-
-			$mainContainer.css(getTransformCssObject(newX));
-
-			displayCurrentValue(e);
-		});
-
-		Hammer(domMainContainer).on('dragstart', function(e) {
-			scrollTopBeforeDrag = $mainContainer.scrollTop();
-		});
-
-		Hammer(domMainContainer).on('dragend', function(e) {
-			e.stopPropagation();
-
-			if (scrollTopBeforeDrag != $mainContainer.scrollTop()) {
-				return;
-			}
-
-			var absDeltaX = Math.abs(e.gesture.deltaX);
-
-			if ($mainContainer.hasClass(menuIsVisibleClass)) {
-				if ((absDeltaX > 100) || (e.gesture.velocityX > 0.3)) {
-					$mainContainer.removeClass(menuIsVisibleClass);
-				}
-			} else if ((e.gesture.direction == 'right') && ((absDeltaX > 130) || (e.gesture.velocityX > 0.3))) {
-				$mainContainer.addClass(menuIsVisibleClass);
-			}
-
-			console.log(e.gesture.direction)
-
-			$mainContainer.css(getTransformCssObject());
-
-			$mainContainer.addClass(transitionClass);
-
-			setTimeout(function() {
-				isDragging = false;
-			}, 100);
-		});
-
-		$('a').on('click', function() {
-			if (isDragging) return false;
-		});
-
-		function getTransformCssObject(val) {
-			var cssVal = val ? 'translateX(' + val + 'px)' : null;
-
-			return {
-				'-ms-transform': cssVal,
-				'-moz-transform': cssVal,
-				'-webkit-transform': cssVal,
-				'transform': cssVal
-			};
+		function openMenu() {
+			$layoutMenuWrap.show();
+			$layoutMain.addClass(layoutMainOpenMenuClass);
+			$layoutOverlay.show();
+			isMenuOpen = true;
 		}
-	});
 
+		function closeMenu() {
+			$layoutMain.removeClass(layoutMainOpenMenuClass);
+			$layoutOverlay.hide();
+			setTimeout(function() {
+				$layoutMenuWrap.hide();
+			}, MENU_TRANSITION_TIME);
+			isMenuOpen = false;
+		}
+
+
+		$layoutMenuToggle.on('click', function() {
+			if (isMenuOpen) {
+				closeMenu();
+			} else {
+				openMenu();
+			}
+		});
+
+		$layoutOverlay.on('click', function() {
+			closeMenu();
+		});
+	});
 
 	window.addEventListener('load', function() {
 	    new FastClick(document.body);
 	}, false);
-
-	function displayCurrentValue(e) {
-		$('#rrr').html(Math.floor(e.gesture.deltaX) + ', ' + Math.floor(e.gesture.velocityX*100)/100 + ', ' + e.gesture.direction);
-	}
 })();
