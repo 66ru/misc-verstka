78db3bfab66e2c9fa3994f5fb24efac532770609
Anton Belyaev <anton.n.belyaev@gmail.com>
2014-06-02 17:34:16 +0600

    Weather: add expand all link

--- weather/js/weather.js
+++ weather/js/weather.js
@@ -1,14 +1,19 @@
 (function($) {
-	var $weatherBlocks = $('.weather').not('.weather_primary');
+	var $weatherBlocks = $('.weather').not('.weather_primary'),
+		$weatherExpandAll = $('.weather__expand-all-text');
 
 	$weatherBlocks.each(function() {
-		var $this = $(this),
-			$rowsToHide = $this.children('.weather__row').not('.weather__row_main');
+		var $this = $(this);
+			// $rowsToHide = $this.children('.weather__row').not('.weather__row_main');
 
 		// $rowsToHide.hide();
 		$this.on('click', function() {
 			$this.toggleClass('weather_collapsed');
 		});
+	});
+
+	$weatherExpandAll.on('click', function() {
+		$weatherBlocks.removeClass('weather_collapsed');
 	});
 
 })(jQuery);
