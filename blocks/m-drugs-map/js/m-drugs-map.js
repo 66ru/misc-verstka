148513d0ab4b0a9d2d5ded3612fcd8435eade5e6
Anton Belyaev <anton.n.belyaev@gmail.com>
2014-08-19 17:51:31 +0600

    #54 Mobile doska: fix goods image size

--- blocks/m-drugs-map/js/m-drugs-map.js
+++ blocks/m-drugs-map/js/m-drugs-map.js
@@ -9,7 +9,13 @@
 			showMapButtonClassName = 'm-drugs-map__map-button',
 			currentButtonClassName = 'm-button_current',
 
-			$window = $(window);
+			$window = $(window),
+			mobileSafari;
+
+			if(/iP/.test(navigator.platform) == /Safari/i.test(navigator.userAgent)) {
+				mobileSafari = true;
+				console.log(42)
+			}
 
 		$('.' + mainBlockClassName).each(function() {
 			var $this = $(this),
@@ -20,8 +26,12 @@
 				$listButton = $this.find('.' + showListButtonClassName),
 				$mapButton = $this.find('.' + showMapButtonClassName),
 
-				windowHeight = $window.height() + 60,
-				mapOffsetTop = $mapButton.offset().top -10;
+				windowHeight = $window.height(),
+				mapOffsetTop = $mapButton.offset().top - 10;
+
+			if (mobileSafari) {
+				windowHeight += 70;
+			}
 
 			$listButton.on('click', function(ev) {
 				$this.removeClass(mainBlockMapShownClassName);
@@ -45,14 +55,15 @@
 
 			$mapWrap.height(windowHeight);
 
-			// $window.on('resize', function() {
-			// 	windowHeight = $window.height();
-			// 	$mapWrap.height(windowHeight);
+			$window.on('resize', function() {
+				windowHeight = $window.height();
+				$mapWrap.height(windowHeight);
+				mapOffsetTop = $mapButton.offset().top - 10;
 
-			// 	if ($this.hasClass(mainBlockMapShownClassName)) {
-			// 		$(window).scrollTop(mapOffsetTop);
-			// 	}
-			// });
+				if ($this.hasClass(mainBlockMapShownClassName)) {
+					$(window).scrollTop(mapOffsetTop);
+				}
+			});
 		});
 	});
 })();
